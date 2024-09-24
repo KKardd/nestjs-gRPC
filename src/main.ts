173efdb18ef8d8182ba668f519e2a306bf99fadb
KKardd <wlstjd4013@gmail.com>
2024-09-25 08:04:49 +0900

    feat: apply gRPC in nest

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'post',
+        protoPath: join(__dirname, '../src/proto/post.proto'),
+        url: 'localhost:3000',
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
